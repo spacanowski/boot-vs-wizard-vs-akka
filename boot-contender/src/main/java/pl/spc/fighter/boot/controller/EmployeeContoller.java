1f6887eae84f07cc21ab2725527bb4fea7b24b3a
spacanowski <spacanowski@gmail.com>
2016-04-21 21:32:31 +0200

    Added proper create response status

--- boot-contender/src/main/java/pl/spc/fighter/boot/controller/EmployeeContoller.java
+++ boot-contender/src/main/java/pl/spc/fighter/boot/controller/EmployeeContoller.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.Timed;
@@ -25,6 +27,7 @@ public class EmployeeContoller {
 
     @Timed
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody @Valid Employee employee) {
         return service.addEmployee(employee);
     }
