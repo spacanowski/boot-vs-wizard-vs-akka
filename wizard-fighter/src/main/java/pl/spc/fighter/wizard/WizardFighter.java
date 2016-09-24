8ec9e7ab163be9827ce7cd502aab7237e25d242d
spacanowski <spacanowski@gmail.com>
2016-09-24 09:25:22 +0200

    Added hikari to dropwizard part

--- wizard-fighter/src/main/java/pl/spc/fighter/wizard/WizardFighter.java
+++ wizard-fighter/src/main/java/pl/spc/fighter/wizard/WizardFighter.java
@@ -3,8 +3,10 @@ package pl.spc.fighter.wizard;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.skife.jdbi.v2.DBI;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import io.dropwizard.Application;
-import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Environment;
 import pl.spc.fighter.wizard.configuration.WizardConfiguration;
 import pl.spc.fighter.wizard.dao.EmployeeDao;
@@ -19,15 +21,26 @@ public class WizardFighter extends Application<WizardConfiguration> {
     public void run(WizardConfiguration configuration, Environment environment) throws Exception {
         environment.jersey().packages("pl.spc");
 
-        final DBIFactory factory = new DBIFactory();
-        final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+        HikariConfig config = createHikariConfig(configuration, environment);
+        HikariDataSource dataSource = new HikariDataSource(config);
+        DBI dbi = new DBI(dataSource);
 
         environment.jersey().register(new AbstractBinder() {
             @Override
             protected void configure() {
-                bind(jdbi.onDemand(EmployeeDao.class)).to(EmployeeDao.class);
+                bind(dbi.onDemand(EmployeeDao.class)).to(EmployeeDao.class);
                 bind(EmployeeService.class).to(EmployeeService.class);
             }
         });
+    }
+
+    private HikariConfig createHikariConfig(WizardConfiguration configuration, Environment environment) {
+        HikariConfig config = new HikariConfig("/hikari.properties");
+        config.setJdbcUrl(configuration.getDataSourceFactory().getUrl());
+        config.setUsername(configuration.getDataSourceFactory().getUser());
+        config.setPassword(configuration.getDataSourceFactory().getPassword());
+        config.setPoolName("HikariPool");
+        config.setMetricRegistry(environment.metrics());
+        return config;
     }
 }
